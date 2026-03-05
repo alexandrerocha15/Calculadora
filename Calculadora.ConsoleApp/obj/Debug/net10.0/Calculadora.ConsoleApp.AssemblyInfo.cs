cf40cf38f8488003e569bc0c1fc604b65c572b7f
Alexandre Rocha <juny14_15@hotmail.com>
2026-03-05 14:09:31 -0300

    Commit v1

--- Calculadora.ConsoleApp/obj/Debug/net10.0/Calculadora.ConsoleApp.AssemblyInfo.cs
+++ Calculadora.ConsoleApp/obj/Debug/net10.0/Calculadora.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f246e83bb132e7d1e7b94a05b105fc682b907d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
